7ee1b9e3ae6745cb5462b50b1680df7b42af290f
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2025-02-25 21:48:14 +0530

    navbar,banner component created

---
+++ react-bs/src/components/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Carousel } from "react-bootstrap"
+import banner from "../images/hero-banner.png";
+
+const Banner = () => {
+  return (
+    <Carousel>
+        <Carousel.Item>
+            <img className="d-block w-100" src={banner} alt="First Slide"  />
+        
+            <Carousel.Caption>
+                <h3>First Slide</h3>
+                <p>Banner description goes here.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img className="d-block w-100"  src={banner} alt="First Slide"  />
+        
+            <Carousel.Caption>
+                <h3>First Slide</h3>
+                <p>Banner description goes here.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+    </Carousel>
+  )
+}
+
+export default Banner;
--- react-bs/src/components/NavBar.jsx
+++ react-bs/src/components/NavBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavBar = () => {
+function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">HN Photography</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="w-100 d-flex justify-content-center align-items-center gap-4"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Services</Nav.Link>
+            <Nav.Link href="#">Blog</Nav.Link>            
+            <Nav.Link href="#">Projects</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+          <Button variant="outline-info" className='d-inline-block' style={{ whiteSpace: "nowrap", padding: "10px 20px" }}>Book Now</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  );
 }
 
-export default NavBar
+export default NavBar;
