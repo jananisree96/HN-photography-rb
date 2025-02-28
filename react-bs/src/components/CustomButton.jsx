eecedebe43f0c79631c1755395feb84591df8373
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2025-02-28 09:51:25 +0530

    custome btn changes

--- react-bs/src/components/CustomButton.jsx
+++ react-bs/src/components/CustomButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 const CustomButton = () => {
   return (
-    <Button size="sm" className='btn px-3 py-2 me-4'>Book now</Button>
+    <Button size="sm" className='main-btn px-3 py-2 me-4 rounded'>Book now</Button>
   )
 }
 
