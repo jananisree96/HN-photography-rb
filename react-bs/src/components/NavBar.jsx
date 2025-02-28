e54a067e5acaf5c17b97f0c0d1aa6914a2bdc0f4
Harinivasan30 <harinivasananandhan@gmail.com>
2025-02-28 21:48:02 +0530

    nav-bar not completed

--- react-bs/src/components/NavBar.jsx
+++ react-bs/src/components/NavBar.jsx
@@ -3,30 +3,33 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CustomButton from './CustomButton';
+
 function NavBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" sticky='top'>
-      <Container fluid>
-        <Navbar.Brand href="#">HN Photography</Navbar.Brand>
+    <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+      <Container fluid className="d-flex align-items-center justify-content-between">
+        {/* Left Side - Brand */}
+        <Navbar.Brand href="#" className="fw-bold">HN Photography</Navbar.Brand>
+
+        {/* Center - Navigation */}
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="w-100 d-flex justify-content-center align-items-center gap-4"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
+        <Navbar.Collapse id="navbarScroll" className="justify-content-center">
+          <Nav className="d-flex align-items-center gap-3">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
             <Nav.Link href="#">Services</Nav.Link>
-            <Nav.Link href="#">Blog</Nav.Link>            
+            <Nav.Link href="#">Blog</Nav.Link>
             <Nav.Link href="#">Projects</Nav.Link>
             <Nav.Link href="#">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+        {/* Right Side - Custom Button (Always in the same row) */}
+        <div className="d-lg-block">
+          <CustomButton />
+        </div>
       </Container>
-      <CustomButton/>
     </Navbar>
   );
 }
-
 export default NavBar;
