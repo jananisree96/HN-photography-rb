0f367237ef777c4bb6062c964e1c9d72d332c05d
Harinivasan30 <harinivasananandhan@gmail.com>
2025-03-02 22:24:26 +0530

    navbar fixed

--- react-bs/src/components/NavBar.jsx
+++ react-bs/src/components/NavBar.jsx
@@ -3,15 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CustomButton from './CustomButton';
+import Image from 'react-bootstrap/Image';
+import logo from '../images/hnlogo.png';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
       <Container fluid className="d-flex align-items-center justify-content-between">
-        {/* Left Side - Brand */}
-        <Navbar.Brand href="#" className="fw-bold">HN Photography</Navbar.Brand>
-
-        {/* Center - Navigation */}
+        <Navbar.Brand href="#" className="fw-bold">
+        <Image src={logo} width={50} height={50} alt="Logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll" className="justify-content-center">
           <Nav className="d-flex align-items-center gap-3">
@@ -23,13 +24,12 @@ function NavBar() {
             <Nav.Link href="#">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-
-        {/* Right Side - Custom Button (Always in the same row) */}
-        <div className="d-lg-block">
+        <div className="d-none d-lg-block">
           <CustomButton />
         </div>
       </Container>
     </Navbar>
   );
 }
+
 export default NavBar;
