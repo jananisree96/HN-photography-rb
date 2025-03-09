f8b9202b0063c0186632c77b923e3695db39ab28
Harinivasan30 <harinivasananandhan@gmail.com>
2025-03-09 20:21:27 +0530

    about-us

--- react-bs/src/components/CubeSlider.jsx
+++ react-bs/src/components/CubeSlider.jsx
@@ -5,53 +5,49 @@ import {EffectCube , Pagination, Autoplay} from 'swiper/modules'
 import 'swiper/css';
 import 'swiper/css/effect-cube';
 import 'swiper/css/pagination';
-import image1 from '../images/img-1.jpg'
 import image2 from '../images/img-2.jpg'
-import image3 from '../images/img-3.jpg'
-import image4 from '../images/img-4.jpg'
-import image5 from '../images/img-5.jpg'
 import image6 from '../images/img-6.jpg'
-
+import image7 from '../images/img-7.jpg'
 const CubeSlider = () => {
   return (
-    <>
-        <Swiper
-            effect={'cube'}
-            grabCursor={true}
-            cubeEffect={{
-                shadow: true,
-                shadowOffset: 20,
-                slideShadows: true,
-                shadowScale: 0.94,
-            }}
-            pagination={true}
-            autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-              }}
-            modules={[EffectCube,Pagination,Autoplay]}
-            className='mySwiper'
-        >
-            <SwiperSlide>
-                <img src={image1} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={image2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={image3} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={image4} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={image5} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={image6} alt="" />
-            </SwiperSlide>
-        </Swiper>
-    </>
+    <div className='container'>
+        <div className="row align-items-center">
+            <div className='col-md-6'>
+                <div className="swiper-container">
+                    <Swiper
+                        effect={'cube'}
+                        grabCursor={true}
+                        cubeEffect={{
+                            shadow: true,
+                            shadowOffset: 20,
+                            slideShadows: true,
+                            shadowScale: 0.94,
+                        }}
+                        pagination={true}
+                        autoplay={{
+                            delay: 4000,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[EffectCube, Pagination, Autoplay]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide><img src={image2} alt="" /></SwiperSlide>
+                        <SwiperSlide><img src={image6} alt="" /></SwiperSlide>
+                        <SwiperSlide><img src={image7} alt="" /></SwiperSlide>
+                    </Swiper>
+                </div>
+            </div>
+
+            <div className='col-md-6'>
+                <div className='about-us'>
+                    <h1>HN Drone Photography</h1><br />
+                    <h3>Inasfa reprehenderit in voluptate velit esse cillum reeut cupidatatfug nulla pariatur.</h3><br />
+                    <p>Aute irure dolor inasfa reprehenderit in voluptate velit esse cillum reeut cupidatatfug nulla pariatur. Excepteur sintxsdfas occaecat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
   )
 }
 
